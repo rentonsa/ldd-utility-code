5fd04365d0f8f43d33aacdee75fead76b6b1142c
Scott Renton <scott.renton@ed.ac.uk>
2018-10-01 18:19:52 +0100

    LUC024A-11 Geddes look and feel changes

--- theme/geddes/views/search_results.php
+++ theme/geddes/views/search_results.php
@@ -1,17 +1,16 @@
 
 <?php
+
 // Set up some variables to easily refer to particular fields you've configured
 // in $config['skylight_searchresult_display']
+
 $title_field = $this->skylight_utilities->getField('Title');
-$author_field = $this->skylight_utilities->getField('Author');
+$author_field = $this->skylight_utilities->getField('Creator');
 $date_field = $this->skylight_utilities->getField('Date');
 $type_field = $this->skylight_utilities->getField('Type');
-$bitstream_field = $this->skylight_utilities->getField('Bitstream');
-$thumbnail_field = $this->skylight_utilities->getField('Thumbnail');
-$abstract_field = $this->skylight_utilities->getField('Abstract');
+$abstract_field = $this->skylight_utilities->getField('Agents');
 $subject_field = $this->skylight_utilities->getField('Subject');
-$link_uri_field = $this->skylight_utilities->getField('Link');
-$image_uri_field = $this->skylight_utilities->getField('ImageUri');
+
 $base_parameters = preg_replace("/[?&]sort_by=[_a-zA-Z+%20. ]+/","",$base_parameters);
 if($base_parameters == "") {
     $sort = '?sort_by=';
@@ -20,153 +19,115 @@ else {
     $sort = '&sort_by=';
 }
 ?>
-<div class="listing-filter">
-        <span class="no-results">
-            <strong><?php echo $startrow ?>-<?php echo $endrow ?></strong> of
-            <strong><?php echo $rows ?></strong> results
-        </span>
 
-        <span class="sort">
-            <strong>Sort by</strong>
-            <?php foreach($sort_options as $label => $field) {
-                if($label == 'Relevancy')
-                {
-                    ?>
-                    <em><a href="<?php echo $base_search.$base_parameters.$sort.$field.'+desc'?>"><?php echo $label ?></a></em>
+<div class="col-md-9 col-sm-9 col-xs-12">
+    <div class="row">
+        <div class="centered text-center">
+            <nav>
+                <ul class="pagination pagination-sm pagination-xs">
                     <?php
-                }
-                else {
-                    ?>
-
-                    <em><?php echo $label ?></em>
-                <?php if($label != "Date") { ?>
-                        <a href="<?php echo $base_search.$base_parameters.$sort.$field.'+asc' ?>">A-Z</a> |
-                <a href="<?php echo $base_search.$base_parameters.$sort.$field.'+desc' ?>">Z-A</a>
-                    <?php } else { ?>
-                        <a href="<?php echo $base_search.$base_parameters.$sort.$field.'+desc' ?>">newest</a> |
-                <a href="<?php echo $base_search.$base_parameters.$sort.$field.'+asc' ?>">oldest</a>
-                    <?php } } } ?>
-
-        </span>
-
-</div>
-
-
-<ul class="listing">
-
-    <?php
-    $j = 0;
-    foreach ($docs as $index => $doc) {
-        ?>
-        <?php
-        $type = 'Unknown';
-        if(isset($doc[$type_field])) {
-            $type = "media-" . strtolower(str_replace(' ','-',$doc[$type_field][0]));
-        }
-        ?>
-
-        <li<?php if($index == 0) { echo ' class="first"'; } elseif($index == sizeof($docs) - 1) { echo ' class="last"'; } ?>>
-            <!--span class="icon <?php echo $type?>"></span-->
-            <div class="item-div">
-
-                <div class = "iteminfo">
-
-                    <?php if(array_key_exists($author_field,$doc)) { ?>
-                        <?php
-                        $num_authors = 0;
-                        foreach ($doc[$author_field] as $author) {
-                            // test author linking
-                            // quick hack that only works if the filter key
-                            $orig_filter = urlencode($author);
-                            $lower_orig_filter = strtolower($author);
-                            $lower_orig_filter = urlencode($lower_orig_filter);
-                            echo '<a class="artist" href="./search/*:*/Artist:%22'.$lower_orig_filter.'%7C%7C%7C'.$orig_filter.'%22">'.$author.'</a>';
-                            $num_authors++;
-                            if($num_authors < sizeof($doc[$author_field])) {
-                                echo ' ';
-                            }
-                        }
-                        ?>
-                    <?php } ?>
-
-
-                    <h3><a href="./record/<?php echo $doc['id']?>?highlight=<?php echo $query ?>"><?php echo $doc[$title_field][0]; ?>
-                            <?php if(array_key_exists($date_field, $doc)) { ?>
-                                <?php
-                                echo '(' . $doc[$date_field][0] . ')';
-                            }
-                            elseif(array_key_exists('dateIssuedyear', $doc)) {
-                                echo '( ' . $doc['dateIssuedyear'][0] . ')';
-                            }
-                            ?></a></h3>
-
-                    <div class="tags">
-
-                        <?php
-                        if(array_key_exists($abstract_field, $doc)) {
-                            echo '<p>';
-                            $abstract =  $doc[$abstract_field][0];
-                            $abstract_words = explode(' ',$abstract);
-                            $shortened = '';
-                            $max = 40;
-                            $suffix = '...';
-                            if($max > sizeof($abstract_words)) {
-                                $max = sizeof($abstract_words);
-                                $suffix = '';
-                            }
-                            for ($i=0 ; $i<$max ; $i++){
-                                $shortened .= $abstract_words[$i] . ' ';
-                            }
-                            echo $shortened.$suffix;
-                            echo '</p>';
-                        }
-                        ?>
-
-                    </div> <!-- close tags div -->
-
-                </div> <!-- close item-info -->
-
-                <div class = "thumbnail-image-search">
-
-                    <?php
-                    $numThumbnails = 0;
-                    $imageset = false;
-                    $thumbnailLink = array();
-                    if (isset($doc[$image_uri_field]))
+                    foreach ($paginationlinks as $pagelink)
                     {
-                        foreach ($doc[$image_uri_field] as $imageUri)
-                        {
-
-                            if (strpos($imageUri, 'luna') > 0)
-                            {
-                                $iiifurlsmall = str_replace('/full/0/', '/!250,250/0/', $imageUri);
-                                $thumbnailLink[$numThumbnails]  = '<a title = "' . $doc[$title_field][0] . '" class="fancybox" rel="group" href="' . $imageUri . '"> ';
-                                $thumbnailLink[$numThumbnails] .= '<img src = "' . $iiifurlsmall . '" class="record-thumbnail-search" title="' . $doc[$title_field][0] . '" /></a>';
-                                $numThumbnails++;
-                                $imageset = true;
-                            }
-                        }
-                        if ($imageset == true) {
-                            echo $thumbnailLink[0];
-                        }
+                        echo $pagelink;
                     }
                     ?>
-                    <!--</div>-->
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="row search-row">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 results-num">
+            <h5 class="text-muted">Showing <?php echo $rows ?> results </h5>
+        </div>
 
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 results-num sort">
+            <h5 class="text-muted">Sort By:
+                <?php foreach($sort_options as $label => $field) {
+                    if($label == 'Relevancy')
+                    {
+                        ?>
+                        <em><a href="<?php echo $base_search.$base_parameters.$sort.$field.'+desc'?>"><?php echo $label ?></a></em>
+                        <?php
+                    }
+                    else {
+                        ?>
+
+                        <em><?php echo $label ?></em>
+                        <?php if($label != "Date") { ?>
+                            <a href="<?php echo $base_search.$base_parameters.$sort.$field.'+asc' ?>">A-Z</a> |
+                            <a href="<?php echo $base_search.$base_parameters.$sort.$field.'+desc' ?>">Z-A</a>
+                        <?php } else { ?>
+                            <a href="<?php echo $base_search.$base_parameters.$sort.$field.'+desc' ?>">newest</a> |
+                            <a href="<?php echo $base_search.$base_parameters.$sort.$field.'+asc' ?>">oldest</a>
+                        <?php } } } ?>
+            </h5>
+        </div>
+
+    </div>
+    <?php
+    foreach ($docs as $index => $doc) {
+        ?>
+        <div class="row search-row">
+            <h3><a href="./record/<?php echo $doc['id']?>/<?php //echo $doc['types'][0]?>"><?php echo strip_tags($doc[$title_field][0]); ?></a></h3>
+
+            <?php
+            if (isset($doc["component_id"])) {
+                $component_id = $doc["component_id"];
+                echo'<div class="component_id">' . $component_id . '</div>';
+            } ?>
+
+            <?php if(array_key_exists($author_field,$doc)) { ?>
+                <?php
+
+                $num_authors = 0;
+                foreach ($doc[$author_field] as $author) {
+                    $orig_filter = urlencode($author);
+
+                    echo '<a class="agent" href="./search/*:*/Agent:%22'.$orig_filter.'%22">'.$author.'</a>';
+                    $num_authors++;
+                    if($num_authors < sizeof($doc[$author_field])) {
+                        echo ' ';
+                    }
+                }
+                ?>
+            <?php } ?>
+
+            <?php if(array_key_exists($subject_field,$doc)) { ?>
+                <div class="tags">
+                    <?php
+
+                    $num_subject = 0;
+                    foreach ($doc[$subject_field] as $subject) {
+
+                        $orig_filter = urlencode($subject);
+                        echo '<a class="subject" href="./search/*:*/Subject:%22'.$orig_filter.'%22">'.$subject.'</a>';
+                        $num_subject++;
+                        if($num_subject < sizeof($doc[$subject_field])) {
+                            echo ' ';
+                        }
+                    }
+
+                    ?>
                 </div>
-                <div class="clearfix"></div>
-            </div> <!-- close item div -->
-        </li>
+            <?php } ?>
+        </div> <!-- close row-->
         <?php
-        $j++;
+
     } // end for each search result
+
     ?>
-</ul>
-
-
-<div class="pagination">
-        <span class="no-results">
-            <strong><?php echo $startrow ?>-<?php echo $endrow ?></strong> of
-            <strong><?php echo $rows ?></strong> results </span>
-    <?php echo $pagelinks ?>
-</div>
+    <div class="row">
+        <div class="centered text-center">
+            <nav>
+                <ul class="pagination pagination-sm pagination-xs">
+                    <?php
+                    foreach ($paginationlinks as $pagelink)
+                    {
+                        echo $pagelink;
+                    }
+                    ?>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div> <!-- close col 9 -->
