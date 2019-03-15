4dcbebc86c80f52a75a36974e2adfe8427d1b5fd
Scott Renton <scott.renton@ed.ac.uk>
2019-03-15 16:11:57 +0000

    LUC023A-116 Conectando Exhibition

---
+++ static/exhibitions/conectando.php
@@ -0,0 +1,12 @@
+<div class="content">
+    <div class="content byEditor">
+        <h1>Conectando: Scottish Encounters with Spanish & Portuguese</a></h1>
+        <div id="conectando-banner"></div>
+        <h2>About the Exhibition</h2>
+        <h3>Opening: 5 April 2019 | Where: Main Library Exhibition Gallery – Ground Floor | Closing: 29 June 2019 </h3>
+        <p>
+            From artists seeking inspiration, to volunteers who fought in the Spanish Civil War, and students learning a new language, discover the many Scottish connections with the Spanish and Portuguese speaking worlds.
+        </p>
+        <a href="https://www.ed.ac.uk/literatures-languages-cultures/delc/splas/news-and-events/conectando" target="_blank" alt="Conectando at LLC" title="Conectando at LLC">Conectando at Literatures, Languages and Cultures</a>
+    </div>
+</div>
