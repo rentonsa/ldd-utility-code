c2c04cff5657d922d10e05b02f706759137885d1
Scott Renton <scott.renton@ed.ac.uk>
2019-04-10 13:24:55 +0100

    LUC023A-117 JLSS initial setup

---
+++ config/jlss.php
@@ -0,0 +1,156 @@
+<?php
+
+$config['skylight_appname'] = 'jlss';
+
+// Uncomment this if you are using a url of the form http://.../art/...
+$config['skylight_url_prefix'] = 'jlss';
+
+// set ga code
+if (strpos($_SERVER['HTTP_HOST'], "test") !== false) {
+    $config['skylight_ga_code'] = 'UA-25737241-6';
+}
+else {
+    $config['skylight_ga_code'] = 'UA-25737241-9';
+}
+
+$config['skylight_adminemail'] = 'is-crc@ed.ac.uk';
+
+$config['skylight_theme'] = 'jlss';
+
+$config['skylight_fullname'] = 'Jewish Lives Scottish Spaces';
+
+$config['skylight_adminemail'] = 'lddt@mlist.is.ed.ac.uk';
+
+$config['skylight_oaipmhcollection'] = '';
+
+$config['skylight_oaipmhallowed'] = true;
+
+
+// Container ID and the field used in solr index to store this ID. Used for restricting search/browse scope.
+$config['skylight_container_id'] = '89';
+$config['skylight_container_field'] = 'location.coll';
+
+$config['skylight_fields'] = array(
+    'Title' => 'dc.title.en',
+    'Alternative Title' => 'dc.title.alternative.en',
+    'Artist' => 'dc.contributor.authorfull.en',
+    'Author' => 'dc.contributor.author.en',
+    'Classification' => 'dc.subject.classification.en',
+    'Type' => 'dc.type.en',
+    'Abstract' => 'dc.description.abstract.en',
+    'Date' => 'dc.coverage.temporal.en',
+    'Bitstream'=> 'dc.format.original.en',
+    'Thumbnail'=> 'dc.format.thumbnail.en',
+    'Description'=>'dc.description.en',
+    'Rights' => 'dc.rights.holder.en',
+    'Accession Number'=> 'dc.identifier.en',
+    'Collection' => 'dc.relation.ispartof.en',
+    'Provenance' => 'dc.description.provenance',
+    'Material' => 'dc.format.en',
+    'Dimensions' => 'dc.format.extent.en',
+    'Signature' => 'dc.format.signature.en',
+    'Inscription' => 'dc.format.inscription.en',
+    'Subject' => 'dc.subject.en',
+    'Place Made' => 'dc.coverage.spatial.en',
+    'Period' => 'dc.coverage.temporalperiod.en',
+    'Link' => 'dc.identifier.uri',
+    'Tags' => 'dc.subject.crowdsourced.en',
+    'ImageUri' => 'dc.identifier.imageUri.en',
+    'Permalink' => 'dc.contributor.authorpermalink.en',
+    'SketchFabURI' => 'dc.identifier.sketchuri.en'
+);
+
+$config['skylight_schema_links'] = array(
+    'Title'=> 'name',
+    'Alternative Title'=> 'alternativeName',
+    'Artist'=> 'creator',
+    'Author'=> 'creator',
+    'Classification'=> 'keywords',
+    'Date'=>'dateCreated',
+    'Thumbnail'=>'thumbnailUrl',
+    'Description'=> 'description',
+    'Rights'=>'copyrightHolder',
+    'Accession Number'=> 'identifier',
+    'Collection'=> 'isPartOf',
+    'Material'=>'material',
+    'Signature'=> 'creator',
+    'Subject'=> 'about',
+    'Place Made'=> 'locationCreated',
+    'Period'=> 'temporalCoverage',
+    'Link'=> 'url',
+    'ImageUri'=> 'image',
+    'Tags'=> 'keywords'
+
+);
+
+$config['skylight_date_filters'] = array();
+$config['skylight_filters'] = array('Artist' => 'author_filter', 'Classification' => 'classification_filter', 'Collection'=> 'collection_filter', 'Period' => 'period_filter', 'Tags' => 'tags_filter');
+
+$config['skylight_filter_delimiter'] = ':';
+
+$config['skylight_meta_fields'] = array(
+    'Title' => 'dc.title.en',
+    'Artist' => 'dc.contributor.authorfull.en',
+    'Description' => 'dc.description.en',
+    'Classification' => 'dc.subject.classification.en',
+    'Date' => 'dc.coverage.temporal.en',
+    'Type' => 'dc.type.en',
+    'Tags' => 'dc.subject.crowdsourced.en',
+);
+
+$config['skylight_recorddisplay'] = array( 'Permalink','Artist','Title','Alternative Title','Date','Period','Description','Material','Dimensions','Type','Place Made','Subject','Collection','Classification','Signature', 'Inscription','Accession Number');
+
+$config['skylight_searchresult_display'] = array('Author','Title','Medium','Type','Description', 'Bitstream', 'Thumbnail', 'Date');
+
+$config['skylight_search_fields'] = array(
+    'Artist' => 'dc.contributor.author.en',
+    'Title' => 'dc.title.en',
+    'Classification' => 'dc.subject.en',
+    'Accession Number'=> 'dc.identifier.en',
+    'Tags' => 'dc.subject.crowdsourced.en'
+);
+
+$config['skylight_related_fields'] = array('Artist' => 'dc.contributor.authorfull.en', 'Subject' => 'dc.subject.en');
+$config['skylight_related_number'] = 5;
+
+$config['skylight_sort_fields'] = array(
+    'Artist' => 'dc.contributor.author_sort ', 'Title' => 'dc.title_sort'
+);
+
+$config['skylight_default_sort'] = 'dc.contributor.author_sort+asc';
+
+$config['skylight_feed_fields'] = array('Title' => 'Title',
+    'Artist' => 'Artist',
+    'Classification' => 'Classification',
+    'Description' => 'Description',
+    'Date' => 'Date');
+
+
+$config['skylight_results_per_page'] = 10;
+$config['skylight_share_buttons'] = false;
+
+$config['skylight_homepage_recentitems'] = false;
+$config['skylight_homepage_randomitems'] = false;
+$config['skylight_related_number'] = 10;
+
+// Set to the number of minutes to cache pages for. Set to false for no caching.
+// This overrides the setting in skylight.php so is commented by Demo
+$config['skylight_cache'] = false;
+
+// Digital object management
+$config['skylight_bitstream_field'] = '';
+$config['skylight_thumbnail_field'] = '';
+$config['skylight_display_thumbnail'] = false;
+$config['skylight_link_bitstream'] = false;
+
+
+// Display common image formats in "light box" gallery?
+$config['skylight_lightbox'] = true;
+$config['skylight_lightbox_mimes'] = array('image/jpeg', 'image/gif', 'image/png');
+
+// Language and locale settings
+$config['skylight_language_default'] = 'en';
+$config['skylight_language_options'] = array('en');
+
+
+?>
---
+++ theme/jlss/views/index.php
@@ -0,0 +1,42 @@
+    <div class="col-md-9 col-sm-9 col-xs-12">
+    <h1 class="itemtitle">Medical records revived: <small>Case Note Catalogues at Lothian Health Services Archive</small></h1>
+        <p>
+            Lothian Health Services Archive (LHSA) is the archive of NHS Lothian, based in the Centre for Research
+            Collections, University of Edinburgh. The Archive collects evidence of the history of healthcare in
+            Edinburgh and the Lothians, from records of how Edinburgh’s NHS hospitals were founded and managed to
+            privately donated collections gifted by individuals and organisations shaping the region’s medical history.
+            LHSA records date from 1594 to the present day.
+
+        </p>
+        <img src="<?php echo base_url()?>/theme/lhsacasenotes/images/P_PL1_B_E_101-RIE-Exterior-1985.gif" alt="Royal Infirmary Edinburgh Exterior" class="img-responsive pull-right img-circle" />
+
+        <p>
+            Patient records are an invaluable way of bringing Edinburgh’s historic breakthroughs in medical treatment and
+            care to light. As well as bound accounts of treatment (dating back to the eighteenth century), LHSA holds over one million
+            twentieth-century patient case notes in folders in one of the largest medical archives in the UK.
+            The majority of these cases (although by no means all) were written in the Royal Infirmary of Edinburgh.
+            They cover both specific medical specialisms (from neurosurgery, respiratory disease and psychiatric care
+            to orthopaedics and obstetrics) and more general medicine and surgery.
+        </p>
+        <p>
+        In 2012, LHSA developed a methodology for cataloguing these modern archival case notes. Catalogues describe
+            individual cases for researchers whilst protecting the privacy of patients through careful
+            redaction of potentially identifying details. This resource acts as a gateway to these anonymised case note
+            catalogues. It will guide you in using the catalogues for the first time, give context to key people,
+            subjects and places, and will provide information for legitimate researchers on applying to access the
+            confidential documents that they describe. At present, finding aids for neurosurgical and tuberculosis case notes can be accessed,
+            but new catalogues will be added as they are completed.
+
+        </p>
+
+        <p>
+        The support of the <a href="https://wellcome.ac.uk/" alt="Welcome Trust" target="_blank">Wellcome Trust</a> is
+            gratefully acknowledged. Research Resources funding has enabled the ongoing
+            cataloguing of Norman Dott’s neurosurgical case notes and those of the Royal Victoria Hospital, the Royal
+            Victoria Dispensary for Diseases of the Chest, the Regional Hospital National X-ray Campaign and Southfield
+            Sanitorium. <a href="http://www.lhsa.lib.ed.ac.uk/projects/Projects-WTProjects.htm" target="_blank">Find out more about these projects here.</a>
+        </p>
+        <img src="<?php echo base_url()?>/theme/lhsacasenotes/images/dott-index-image.gif" alt="Norman Dott Case Notes" class="img-responsive pull-right index-img" />
+
+
+    </div>
