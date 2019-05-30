05e457fb2c29223d9ede62b51238b18231ea4900
Scott Renton <scott.renton@ed.ac.uk>
2019-05-30 10:56:58 +0100

    LUC023A-126 Art About page

--- static/art/about.php
+++ static/art/about.php
@@ -1,75 +1,68 @@
 <div class="content">
     <div class="content byEditor">
-        <h1>About the University of Edinburgh Art Collection</h1>
+        <p>Our Art Collection supports the world-leading research and teaching that happens within the University.
+            Comprised of an astonishing range of objects and ideas spanning two millennia and a multitude of artistic forms,
+            the collection reflects not only the long and rich trajectory of the University, but also major national and international shifts in art history.
+        </p>
 
-        <p>The University holds around 10,000 works of art. The Art Collection is now an amalgamation of the University
-            of Edinburgh’s original art collection, which spans some 400 years of collecting, and the Edinburgh College
-            of Art collection of prints, drawings, paintings and sculpture. The ECA collection tells a unique story of
-            the artist output of the College, particularly in the early to mid-20th Century. Some of the most respected
-            names in Scottish art appear, such as <a href="./search/Peploe">Samuel Peploe</a>, <a href="./search/Bellany">John Bellany</a>,
-            <a href="./search/Redpath">Anne Redpath</a> and <a href="./search/Blackadder">Elizabeth Blackadder</a>.</p>
-        <p>
-            The Art Collection has as its centrepiece the Torrie Collection of 17th century Dutch and Italian Masters
-            with works by <a href="./search/Ruisdael">Ruisdael</a>, <a href="./search/ten%20Oever">ten Oever</a>,
+        <p>A significant and celebrated component of our holdings is the Torrie collection, which features seventeenth-century Dutch and Italian work,
+            with examples by <a href="./search/Ruisdael">Ruisdael</a>, <a href="./search/ten%20Oever">ten Oever</a>,
             <a href="./search/Van%20der%20Meulen">Van der Meulen</a>, <a href="./search/Pynacker">Pynacker</a>,
-            <a href="./search/Rosa">Rosa</a> and <a href="./search/Rvan%20de%20Velde">Rvan de Velde</a>. The University also holds
-            the second largest collection of portraits in Scotland ranging from 17th century portraits of
-            <a href="./search/Napier">John Napier</a> and <a href="./search/John%20Knox">John Knox</a>
-            to the recent dynamic painting of <a href="./search/Peter%20Higgs">Peter Higgs</a>
-            by Ken Currie. Other portrait artists represented are
-            <a href="./search/Henry%20Raeburn">Sir Henry Raeburn</a>, <a href="./search/Stanley%20Cursiter">Stanley Cursiter</a>,
+            <a href="./search/Rosa">Rosa</a> and <a href="./search/van%20de%20Velde">van de Velde</a>. The University also holds the second largest collection of
+            portraits in Scotland, ranging from historic depictions of <a href="./search/Napier">John Napier</a> and <a href="./search/John%20Knox">John Knox</a>,
+            to the more recent painting of Nobel Laureate <a href="./search/Peter%20Higgs">Peter Higgs</a> by
+            Ken Currie. Other portrait artists represented include <a href="./search/Henry%20Raeburn">Sir Henry Raeburn</a>, <a href="./search/Stanley%20Cursiter">Stanley Cursiter</a>,
             <a href="./search/George%20Reid">Sir George Reid</a>, <a href="./search/James%20Cowie">James Cowie</a> and
             <a href="./search/Victoria%20Crowe">Victoria Crowe</a>.
         </p>
-        <p>
-            A large percentage of the Art Collection is on display enhancing the public, staff and student spaces of the
-            University. A number of works from the Torrie Collection are on long-term loan to the National Galleries of Scotland.
-            The principal areas of display include Edinburgh College of Art, Old College, Playfair Library, Raeburn Room,
-            the University of Edinburgh Library, New College, McEwan Hall and the Royal (Dick) School of Veterinary Studies.
+
+        <p>After the organisations merged in 2011, the Edinburgh College of Art (ECA) collection of prints, drawings, paintings and sculpture also became
+            part of the wider University Art Collection. These works help tell the story of the artistic output of the College, with a particular strength being the
+            early to mid-20th Century work by artists such as <a href="./search/Peploe">Samuel Peploe</a>, <a href="./search/Bellany">John Bellany</a>,
+            <a href="./search/Redpath">Anne Redpath</a> and <a href="./search/Blackadder">Elizabeth Blackadder</a>.
         </p>
+
         <p>
-            As well as supporting research at all levels of the University, we believe that the uniqueness of our art collections
-            lies in their innovative use. We seek to interpret and display collections in new and exciting ways, introducing
-            contemporary insights and conversations to constantly reinvigorate debate. This involves working closely with
-            Edinburgh College of Art and The Talbot Rice Gallery to ensure that collections are developed through contemporary
-            collecting and also utilised by some of the world’s leading contemporary artists, academics and curators and
-            presented to as wide an audience as possible.
+            We continue to actively acquire works for the collection, with the most recent strand of development being the Contemporary Art Research Collection (CARC).
+            Established in 2015 in partnership with academics in the History of Art department at ECA, the CARC takes globalisation as its central theme, setting a
+            specific focus on women's experience and the contribution of feminist thought. Artists acquired to date include <a href="./search/Whittle">Alberta Whittle</a>,
+            <a href="./search/McNaughton">Shona McNaughton</a>, <a href="./search/Kate%20Davis">Kate Davis</a>,
+            <a href="./search/Gilligan">Melanie Gilligan</a> and <a href="./search/Bauer">Petra Bauer</a>.
         </p>
 
 
-        <h1>Art Collections Online</h1>
-        <p>
-            We aim to represent all of the University Art Collection online through this resource. This is an ongoing process as the existing collection continues to be catalogued and photographed and also as new acquisitions are brought in. Images accompany records where there is an existing image file of sufficient quality and images will be added to existing records as part of wider cataloguing efforts. If there is a particular work that is of interest that does not have an accompanying image, please contact us directly on the details below.
-        </p>
-        <h1>University Collections</h1>
-        <p>
-            Special Collections include manuscripts and archives, rare books, photographs, printed and handwritten music, architectural drawings, theses and microfilms. The collections relate to University of Edinburgh and its history, and to Scotland and the wider world.</p>
-        <p>In total we have about 35 kilometres of historic material, much of which cannot be found anywhere else in the world. <a href="http://collections.ed.ac.uk" Title="University Collections Home">collections.ed.ac.uk</a>
-        </p>
-        <h1>Centre for Research Collections</h1>
-        <p>
-            Further information about the <a href="http://www.ed.ac.uk/schools-departments/information-services/library-museum-gallery/crc" title="Centre for Research Collections" target="_blank">Centre for Research Collections</a>.
-        </p>
-        <h1>BBC Your Paintings</h1>
-        <p>
-        <a href="http://www.bbc.co.uk/arts/yourpaintings/galleries/locations/university-of-edinburgh-5095" title="" >University Collections on BBC Your Paintings</a><br/>
-        <a href="http://www.bbc.co.uk/arts/yourpaintings/galleries/locations/eca-part-of-university-of-edinburgh-fine-art-collection-7553" title="" >Edinburgh College of Art Collections on BBC Your Paintings</a>
-        <p/>
+        <h1>Display, loan and access</h1>
+
+        <p>A large portion of the Art Collection is on view throughout the University campus, and we actively lend to other venues where possible. Viewing works held in store,
+            or enquiring about borrowing a work, can be arranged by contacting <a href="mailto:is-crc@ed.ac.uk">is-crc@ed.ac.uk</a></p>
+
+        <h1>Online</h1>
+
+        <p>Representing the University Art Collection online is an ongoing process. Images accompany records where there is an existing image file of sufficient quality, and
+            images will be added to records as part of wider cataloguing efforts. If there is a particular work that is of interest that does not have an accompanying image, please contact us directly on the
+            details below.</p>
+
         <h1>Contact Details</h1>
         <p>
             For further information about the University of Edinburgh Art Collections please contact:
         </p>
-        <p><strong>Julie-Ann Delaney</strong><br />
-            Art Collections Curator<br />
+        <p>Art Collections Curator<br />
             University of Edinburgh,<br />
             Main Library,<br />
             30 George Square,<br />
             Edinburgh,<br />
             EH8 9LJ<br />
-            <a class="email" href="mailto:julie-ann.delaney@ed.ac.uk">julie-ann.delaney@ed.ac.uk</a><br />
-            <br />
-            <a href="https://twitter.com/UoEArtArchives" title="Art Collections on Twitter">Art Collections on Twitter</a><br />
-            <a href="http://uoeartandarchives.tumblr.com/" title="Art Collections on Tumblr">Art Collections on Tumblr</a><br /></p>
+            <a class="email" href="mailto:is-crc@ed.ac.uk">is-crc@ed.ac.uk</a><br />
+        </p>
+
+
+
+        <p>Visit Centre for Research Collections <a href="http://www.ed.ac.uk/schools-departments/information-services/library-museum-gallery/crc" title="Centre for Research Collections" target="_blank">webpage</a>
+            for further information about access.</p>
+
+        <a href="https://twitter.com/UoEArtArchives" title="Art Collections on Twitter" target="_blank">Art Collections on Twitter</a><br />
+
+        <a href="https://artuk.org/visit/venues/university-of-edinburgh-5095"  title="Art Collections on ArtUK"  target="_blank">Art Collections on Art UK</a>
 
     </div>
 </div>
