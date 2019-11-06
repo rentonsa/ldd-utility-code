d3a9e6afdb713bf7ac60a24967a41337188fc055
Scott Renton <scott.renton@ed.ac.uk>
2019-11-06 11:54:10 +0000

    LUC023A-156 getting index.php in sync with git

--- theme/coimbra-colls/views/index.php
+++ theme/coimbra-colls/views/index.php
@@ -1,36 +1,35 @@
 <div class="content">
     <!-- todo replace two with sass or less -->
-<div>
-    <div class="parallax img0 inactive"></div>
-    <div class="parallax img1 inactive"></div>
-    <div class="parallax img2 inactive"></div>
-    <div class="parallax img3 inactive"></div>
-    <div class="parallax img4 inactive"></div>
-    <div class="parallax img5 inactive"></div>
-    <div class="parallax img6 inactive"></div>
-    <div class="parallax img7 inactive"></div>
-    <div class="parallax img8 inactive"></div>
-    <div class="parallax img9 inactive"></div>
-    <div class="parallax img10 inactive"></div>
-    <div class="parallax img11 inactive"></div>
-    <div class="parallax img12 inactive"></div>
+    <div>
+        <div class="parallax img0 inactive"></div>
+        <div class="parallax img1 inactive"></div>
+        <div class="parallax img2 inactive"></div>
+        <div class="parallax img3 inactive"></div>
+        <div class="parallax img4 inactive"></div>
+        <div class="parallax img5 inactive"></div>
+        <div class="parallax img6 inactive"></div>
+        <div class="parallax img7 inactive"></div>
+        <div class="parallax img8 inactive"></div>
+        <div class="parallax img9 inactive"></div>
+        <div class="parallax img10 inactive"></div>
+        <div class="parallax img11 inactive"></div>
+        <div class="parallax img12 inactive"></div>
 
 
 
-    <div class="main-categories">
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22anthropology%7C%7C%7CAnthropology%22'>Anthropology</a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22antiquities%7C%7C%7CAntiquities%22'><div class="border-top"></div>Antiquities</a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22art%7C%7C%7CArt%22'><div class="border-top"></div>Art</a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22early+printed+books%7C%7C%7CEarly+printed+books%22'><div class="border-top"></div>Early books</a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22language+and+languages%7C%7C%7CLanguage+and+languages%22'><div class="border-top"></div>Language</a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22manuscripts%7C%7C%7CManuscripts%22'><div class="border-top"></div>Manuscripts</a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22medicine%7C%7C%7CMedicine%22'><div class="border-top"></div>Medicine</a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22musical+instruments%7C%7C%7CMusical+Instruments%22'><div class="border-top"></div>Music</a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22natural+history%7C%7C%7CNatural+history%22'><div class="border-top"></div>Natural history</a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22politics+and+government%7C%7C%7CPolitics+and+government%22'><div class="border-top"></div>Politics</a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22social+history%7C%7C%7CSocial+History%22'><div class="border-top"></div>Social History</a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22science%7C%7C%7CScience%22'><div class="border-top"></div>Science</a>
-        <a class="col-xs-12 col-md-12" href='./search/*:*/Category:%22technology%7C%7C%7CTechnology%22'><div class="border-top"></div>Technology</a>
+        <div class="main-categories">
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22anthropology%7C%7C%7CAnthropology%22'>Anthropology</a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22antiquities%7C%7C%7CAntiquities%22'><div class="border-top"></div>Antiquities</a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22art%7C%7C%7CArt%22'><div class="border-top"></div>Art</a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22early+printed+books%7C%7C%7CEarly+printed+books%22'><div class="border-top"></div>Early books</a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22language+and+languages%7C%7C%7CLanguage+and+languages%22'><div class="border-top"></div>Language</a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22manuscripts%7C%7C%7CManuscripts%22'><div class="border-top"></div>Manuscripts</a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22medicine%7C%7C%7CMedicine%22'><div class="border-top"></div>Medicine</a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22musical+instruments%7C%7C%7CMusical+Instruments%22'><div class="border-top"></div>Music</a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22natural+history%7C%7C%7CNatural+history%22'><div class="border-top"></div>Natural history</a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22politics+and+government%7C%7C%7CPolitics+and+government%22'><div class="border-top"></div>Politics</a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22social+history%7C%7C%7CSocial+History%22'><div class="border-top"></div>Social History</a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22science%7C%7C%7CScience%22'><div class="border-top"></div>Science</a>
+            <a class="col-xs-12 col-md-12" href='./search/*:*/Category:%22technology%7C%7C%7CTechnology%22'><div class="border-top"></div>Technology</a>
+        </div>
     </div>
-</div>
-
