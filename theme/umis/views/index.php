e6b84339d6da69244b28f8e62f3effb96d7c5c57
Scott Renton <scott.renton@ed.ac.uk>
2019-12-19 12:51:56 +0000

    LUC023A-162 UMIS content

--- theme/umis/views/index.php
+++ theme/umis/views/index.php
@@ -15,25 +15,35 @@
     <div class="parallax img11 inactive"></div>
     <div class="parallax img12 inactive"></div>
 
+    <div class="intro">
 
-
-    <!--<div class="main-categories">
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22animals%7C%7C%7CAnimals%22'><div class="border-top"></div><p>Aberdeen</p></a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22architecture-buildings%7C%7C%7CArchitecture-Buildings%22'><div class="border-top"></div><p>Abertay</p></a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22books-printing%7C%7C%7CBooks-Printing%22'><div class="border-top"></div><p>Dundee</p></a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22church-religion%7C%7C%7CChurch-Religion%22'><div class="border-top"></div><p>Edinburgh</p></a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22coins-medals%7C%7C%7CCoins-Medals%22'><div class="border-top"></div><p>Royal Coll Surgeons</p></a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22design%7C%7C%7CDesign%22'><div class="border-top"></div><p>Glasgow</p></a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22dinosaurs%7C%7C%7CDinosaurs%22'><div class="border-top"></div><p>Glasgow Cal</p></a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22engineering%7C%7C%7CEngineering%22'><div class="border-top"></div><p>GSA</p></a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22the-earth%7C%7C%7CThe-Earth%22'><div class="border-top"></div><p>Heriot Watt</p></a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22local-scottish-history%7C%7C%7CLocal-Scottish-History%22'><div class="border-top"></div><p>RGU</p></a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22maps-plans%7C%7C%7CMaps-Plans%22'><div class="border-top"></div><p>St Andrews</p></a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22medicine%7C%7C%7CMedicine%22'><div class="border-top"></div><p>Stirling</p></a>
-        <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22music%7C%7C%7CMusic%22'><div class="border-top"></div><p>West of Scot</p></a>
-    </div>-->
+    </div>
 
     <div class="main-categories">
+            <p class="col-xs-12 col-md-12" id = "warning">THIS SITE IS IN BETA TESTING. PLEASE DO NOT TREAT IT AS A TRUSTED RESOURCE.<br>
+            <span id = "intro">Explore some of the fascinating objects that can be found in Scotland’s university museums.</span></p>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22weird%22'><div class="border-top"></div><p>Weirdest Objects</p></a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22favourite%22'><div class="border-top"></div><p>Favourite Objects</p></a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22burnsg%22'><div class="border-top"></div><p>Burns Night</p></a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22student%22'><div class="border-top"></div><p>Students' Choices</p></a>
+    </div>
+        <!--<div class="main-categories">
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22animals%7C%7C%7CAnimals%22'><div class="border-top"></div><p>Aberdeen</p></a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22architecture-buildings%7C%7C%7CArchitecture-Buildings%22'><div class="border-top"></div><p>Abertay</p></a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22books-printing%7C%7C%7CBooks-Printing%22'><div class="border-top"></div><p>Dundee</p></a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22church-religion%7C%7C%7CChurch-Religion%22'><div class="border-top"></div><p>Edinburgh</p></a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22coins-medals%7C%7C%7CCoins-Medals%22'><div class="border-top"></div><p>Royal Coll Surgeons</p></a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22design%7C%7C%7CDesign%22'><div class="border-top"></div><p>Glasgow</p></a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22dinosaurs%7C%7C%7CDinosaurs%22'><div class="border-top"></div><p>Glasgow Cal</p></a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22engineering%7C%7C%7CEngineering%22'><div class="border-top"></div><p>GSA</p></a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22the-earth%7C%7C%7CThe-Earth%22'><div class="border-top"></div><p>Heriot Watt</p></a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22local-scottish-history%7C%7C%7CLocal-Scottish-History%22'><div class="border-top"></div><p>RGU</p></a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22maps-plans%7C%7C%7CMaps-Plans%22'><div class="border-top"></div><p>St Andrews</p></a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22medicine%7C%7C%7CMedicine%22'><div class="border-top"></div><p>Stirling</p></a>
+            <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22music%7C%7C%7CMusic%22'><div class="border-top"></div><p>West of Scot</p></a>
+        </div>-->
+
+   <!-- <div class="main-categories">
         <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22animals%7C%7C%7CAnimals%22'><div class="border-top"></div><p>THIS</p></a>
         <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22architecture-buildings%7C%7C%7CArchitecture-Buildings%22'><div class="border-top"></div><p>SITE</p></a>
         <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22books-printing%7C%7C%7CBooks-Printing%22'><div class="border-top"></div><p>IS</p></a>
@@ -47,6 +57,6 @@
         <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22maps-plans%7C%7C%7CMaps-Plans%22'><div class="border-top"></div><p>AS</p></a>
         <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22medicine%7C%7C%7CMedicine%22'><div class="border-top"></div><p>TRUSTED</p></a>
         <a class="col-xs-6 col-md-6" href='./search/*:*/Category:%22music%7C%7C%7CMusic%22'><div class="border-top"></div><p>RESOURCE</p></a>
-    </div>
+    </div>-->
 </div>
 
