2a5b09b68adfee19cad53eeb52b6baefa368a91e
Ben Parkes <benjamin.b.parkes@googlemail.com>
2020-01-06 11:43:11 +0000

    Adding additional event files

---
+++ static/exhibitions/embroiderytalkevent.php
@@ -0,0 +1,40 @@
+
+<div class="content" id="static-exhibition">
+    <div class="content byEditor">
+
+    <h1 alt="event title">Talk and Object Handling Session with Lindy Richardson</h1>
+
+    <div class="event-list">
+
+        <div>
+            <div class="anchor" id="firstpaevent-anchor"></div>
+
+            <div class="event-image-box">
+                <img class="event-image" src="<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/images/exhibitboxes/box_2.jpg" alt="event logo" title="Body Language logo">
+            </div>
+
+                <p class="event-p" alt="event description">
+                    Discover the history of the Needlework Development Scheme and its uses today and participate in an object handling session using the collections.
+                </p>
+
+                <p id="info-gap" class="right-align-p" alt="event location and dates">                       
+                    1 FEBRUARY 2020 11:00 - 12:30<br>
+                    <br>
+                    5 FEBRUARY 2020 13:30 - 15:00<br>
+                    <br>
+                    FREE ENTRY. To book and for more information email <a href="mailto: museums@ed.ac.uk">museums@ed.ac.uk</a>
+                </p>
+
+                <a id="info-gap" href="mailto: museums@ed.ac.uk" alt="book or buy tickets link" title="External link to book space on event" traget="_blank">
+                    <button id="eventlist-button" class="exhibit-button">
+                        <p>Email Museums</p>
+                    </button>
+                </a>     
+        </div>
+
+
+    </div>
+        
+        
+    </div>
+</div>
---
+++ static/exhibitions/embroiderytoursevent.php
@@ -0,0 +1,46 @@
+
+<div class="content" id="static-exhibition">
+    <div class="content byEditor">
+
+    <h1 alt="event title">Touching Stitches: Exhibition Tours</h1>
+
+    <div class="event-list">
+
+        <div>
+            <div class="anchor" id="firstpaevent-anchor"></div>
+
+            <div class="event-image-box">
+                <img class="event-image" src="<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/images/exhibitboxes/box_2.jpg" alt="event logo" title="Body Language logo">
+            </div>
+
+                <p class="event-p" alt="event description">
+                    Join us for a 30 minute tour of the exhibition on any of the above dates. A member of staff will also be present in the 
+                </p>
+
+                <p id="info-gap" class="right-align-p" alt="event location and dates">                       
+                    EXHIBITION GALLERY
+                    <br>
+                    GROUND FLOOR, MAIN LIBRARY<br>
+                    <br>
+                    11 DECEMBER 2019 14:30<br>
+                    <br>
+                    15 JANUARY 2020 14:30<br>
+                    <br>
+                    12 FEBRUARY 2020 14:30 | 26 FEBRUARY 2020 14:30<br>
+                    <br>
+                    FREE ENTRY. To book and for more information email <a href="mailto: museums@ed.ac.uk">museums@ed.ac.uk</a>
+                </p>
+                
+                <a id="info-gap" href="mailto: museums@ed.ac.uk" alt="book or buy tickets link" title="External link to book space on event" traget="_blank">
+                    <button id="eventlist-button" class="exhibit-button">
+                        <p>Email Museums</p>
+                    </button>
+                </a>     
+        </div>
+
+
+    </div>
+        
+        
+    </div>
+</div>
