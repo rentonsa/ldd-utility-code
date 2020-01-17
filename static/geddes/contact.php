a724cc5c2301e7d14fec8bfba794d3f91c3eac2d
Scott Renton <scott.renton@ed.ac.uk>
2020-01-17 16:25:46 +0000

    LUC023A-164 Geddes changes

---
+++ static/geddes/contact.php
@@ -0,0 +1,47 @@
+<div class="content">
+    <div class="content byEditor about">
+
+        <p>If you wish advice or information in any of the following areas, the most appropriate contacts are listed below.</p>
+
+        <table>
+        <tr>
+            <td><br>
+                For enquiries relating to the Patrick Geddes Archives collections held at the University of Edinburgh&rsquo;s
+                Centre for Research Collections, and for enquiries relating to the &lsquo;Evergreen: Patrick Geddes and the Environment in Equilibrium&rsquo; project.
+            </td>
+            <td>&nbsp;</td>
+            <td><br>
+                <b>University of Edinburgh Centre for Research Collections</b><br>
+                Main Library George Square<br>
+                Edinburgh <br>
+                EH8 9LJ <br>
+                Phone: +44 (0)131 650 8379 <br>
+                Fax: +44 (0)131 650 2922 <br>
+                Email: <a href="mailto:is-crc@ed.ac.uk" title = "Mail Edinburgh CRC">is-crc@ed.ac.uk</a> <br>
+                Web: <a href="https://www.ed.ac.uk/information-services/library-museum-gallery/crc/about" target="_blank" title="Edinburgh CRC" alt-text="Edinburgh CRC">https://www.ed.ac.uk/information-services/library-museum-gallery/crc/about</a><br><br>
+            </td>
+        </tr>
+        <tr>
+            <td><br>For enquiries relating to Patrick Geddes Archives collections held at the University of Strathclyde Archives and Special Collections.
+            </td>
+            <td>&nbsp;</td>
+            <td><br><b>University of Strathclyde Archives and Special Collections</b><br>
+                Andersonian Library <br>
+                Curran Building<br>
+                101 St James Road <br>
+                Glasgow <br>
+                G4 0NS <br>
+                Phone: +44 (0)141 548 2497 <br>
+                Email: <a href="mailto:archives@strath.ac.uk" title = "Mail Strathclyde Archives">archives@strath.ac.uk</a> <br>
+                Web: <a href="https://www.strath.ac.uk/professionalservices/archives/" target="_blank" title="Strathclyde Archives" alt-text="Strathclyde Archives"> https://www.strath.ac.uk/professionalservices/archives/</a><br><br>
+            </td>
+        </tr>
+        <tr>
+            <td><br>For feedback and suggestions relatingspecifically relating to website content. <br><br></td>
+            <td>&nbsp;</td>
+            <td><br><b>University of Edinburgh Library and Digital Development.</b>
+                <br>Email: <a href="mailto:lddt@mlist.is.ed.ac.uk" title = "Mail Library Digital Development">lddt@mlist.is.ed.ac.uk</a><br><br></td>
+        </tr>
+        </table>
+    </div>
+</div>
