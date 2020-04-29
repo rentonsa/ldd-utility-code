baea157da96d450e9816cddae61cea61717771a9
Ben Parkes <benjamin.b.parkes@googlemail.com>
2020-04-29 16:59:37 +0100

    Style: Hover Animations

---
+++ theme/bodylanguage/views/index.php
@@ -0,0 +1,61 @@
+<div class="record">
+
+    <div id="collection-search">
+        <form action="./redirect/" method="post">
+            <fieldset class="search">
+                <input type="text" name="q" value="<?php if (isset($searchbox_query)) echo urldecode($searchbox_query); ?>" id="q" />
+                <input type="submit" name="submit_search" class="btn" value="Search" id="submit_search" />
+                </fieldset>
+        </form>
+    </div>
+
+    <div class="content byEditor">
+
+        <h1>Body Language: movement, dance and physical education in Scotland, 1890-1990</h1>
+
+        <p>
+            A new Wellcome Trust Research Resources-funded project between Edinburgh University Library Special Collections and the Fergusson Gallery, 
+            Perth  with the support of Moray House School of Education, Margaret Morris Movement International and Scottish Gymnastics.
+        </p>
+
+        <div>
+            <div title="Suzanne Lenglen and Margaret Morris demonstrating a tennis exercise, c.1937. Margaret Morris Collection, Fergusson Gallery, Perth" id="dancer-image"></div>
+            <div title="Dancer Jack Skinner and cricketer Donald Bradman. Illustrations from the prospectus of the Basic Physical Training Association, 1938. Margaret Morris Collection, Fergusson Gallery, Perth" id="cricket-image"></div>
+        </div>
+
+        <div class="clearfix"></div>
+
+        <p class="quote"> 
+            <q>
+                The performance relieves the person temporarily of the burden of being themselves.
+            </q> 
+        </p>
+        <p class="quote-p">
+            - Emma Byrne, Evening Standard.
+        </p>
+
+
+        <p>
+            Over the next two years the project will be cataloguing, preserving and making available significant collections relating to movement, 
+            dance, physical education and gymnastics in Scotland including: 
+        </p>
+        <div class="cataglogue-list-index">
+            <ul class="catalogue-list">
+                <li>
+                    <a>The archives of Margaret Morris Movement International</a>
+                    <div class="overlay"></div>
+                </li>
+                <li>
+                    <a>The records of Dunfermline College of Physical Education</a>
+                    <div class="overlay"></div>
+                </li>
+                <li>
+                    <a>The archives of Scottish Gymnastics</a>
+                    <div class="overlay"></div>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+    <div class="clearfix"></div>
+</div>
