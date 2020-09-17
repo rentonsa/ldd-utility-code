6342002633e2e438b5e43a9c84b438bebe75d480
WebSepp <bastilange@t-online.de>
2020-09-17 13:02:51 +0100

    addes accessibilit statements

--- theme/fairbairn/views/div_main_end.php
+++ theme/fairbairn/views/div_main_end.php
@@ -13,7 +13,7 @@
                     <p><a href="https://www.ed.ac.uk/about/website/privacy" title="Privacy and Cookies Link"  target="_blank">Privacy &amp; Cookies</a>
                         &nbsp;&nbsp;<a href="./takedown" title="Takedown Policy Link">Takedown Policy</a>
                         &nbsp;&nbsp;<a href="./licensing" title="Licensing and Copyright Link">Licensing &amp; Copyright</a>
-                        &nbsp;&nbsp;<a href="https://www.ed.ac.uk/about/website/accessibility" title="Website Accessibility Link" target="_blank">Accessibility</a></p>
+                        &nbsp;&nbsp;<a href="./accessibility" title="Website Accessibility Link" target="_blank">Accessibility</a></p>
                     <p class="footer-copyright">Unless explicitly stated otherwise, all material is copyright &copy; <?php echo date("Y"); ?> <a href="https://www.ed.ac.uk" title="University of Edinburgh Home" target="_blank">University of Edinburgh</a> or <a href="http://www.nls.ac" title="National Library of Scotland Home" target="_blank">National Library of Scotland</a>.</p>
                 </div>
             </div>
