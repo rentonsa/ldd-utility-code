684bdf5f82fe05135693a90b48b6394e8397ede6
WebSepp <bastilange@t-online.de>
2020-09-19 22:02:05 +0100

    fixed footer bug on physics

---
+++ theme/physics/views/div_main_end.php
@@ -0,0 +1,12 @@
+<footer>
+    <div class="footer-disclaimer">
+        <div class="footer-policies">
+            <p><a href="https://www.ed.ac.uk/about/website/privacy" title="Privacy and Cookies Link"  target="_blank">Privacy &amp; Cookies</a>
+                &nbsp;&nbsp;<a href="./takedown" title="Takedown Policy Link">Takedown Policy</a>
+                &nbsp;&nbsp;<a href="./licensing" title="Licensing and Copyright Link">Licensing &amp; Copyright</a>
+                &nbsp;&nbsp;<a href="./accessibility" title="Website Accessibility Link" target="_blank">Accessibility</a></p>
+        </div>
+    </div>
+
+</footer>
+</div>
