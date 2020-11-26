87a98f19d44c45dac9ccff1f29428d5a318c2dbb
Scott Renton <rentonsa@gmail.com>
2020-11-26 11:47:36 +0000

    LUC023A-207 amend healthcheck- remove the fail if no items (there are no items)

--- theme/pointsofarrival/views/healthcheck.php
+++ theme/pointsofarrival/views/healthcheck.php
@@ -9,10 +9,6 @@
         $errors = true;
         $errorMessage = "Unable to connect to solr.";
     }
-    else if(!count($docs) > 0) {
-        $errors = true;
-        $errorMessage = "There are no items in this collection.";
-    }
 
     ?>
 
